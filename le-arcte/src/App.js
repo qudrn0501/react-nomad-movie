0bdbb693c6429c12caedb69f7394b1fac4f993f0
qudrn0501 <104612845+qudrn0501@users.noreply.github.com>
2023-02-14 01:34:34 +0900

    6.3 useEffect recap

--- le-arcte/src/App.js
+++ le-arcte/src/App.js
@@ -1,13 +1,37 @@
-import Button from "./Button";
-import styles from "./App.module.css"
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  console.log("i run all the time");
+
+  useEffect(() => {
+    // useEffect는 코드가 한번만 실행될 수 있도록 보호함
+    console.log("i run only once");
+  }, []);
+  useEffect(() => {
+    console.log("i run when 'keyword' changes.", keyword);
+  }, [keyword])
+  useEffect(() => {
+    console.log("i run when 'counter' changes.", keyword);
+  }, [counter])
+  useEffect(() => {
+    console.log("i run when keyword & counter change");
+  }, [keyword, counter]);
   return (
     <div>
-      <h1 className={styles.title}>welcome back!</h1>
-      <Button text={"Continue"} />
+      <input 
+        value={keyword} 
+        onChange={onChange} 
+        type="text" 
+        placeholder="Search here..." 
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
 
-export default App;
+export default App
