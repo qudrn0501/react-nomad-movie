233178179b746da8a64e1fb4b98c088b386de99d
qudrn0501 <104612845+qudrn0501@users.noreply.github.com>
2023-02-14 01:58:45 +0900

    6.4 cleanup

--- le-arcte/src/App.js
+++ le-arcte/src/App.js
@@ -1,37 +1,25 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("")
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
-
+function Hello() {
   useEffect(() => {
-    // useEffect는 코드가 한번만 실행될 수 있도록 보호함
-    console.log("i run only once");
+    console.log("hi :)");
+    return function(){
+      console.log("bye :(");
+    }
   }, []);
-  useEffect(() => {
-    console.log("i run when 'keyword' changes.", keyword);
-  }, [keyword])
-  useEffect(() => {
-    console.log("i run when 'counter' changes.", keyword);
-  }, [counter])
-  useEffect(() => {
-    console.log("i run when keyword & counter change");
-  }, [keyword, counter]);
+  return <h1>hello</h1>
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
-    <div>
-      <input 
-        value={keyword} 
-        onChange={onChange} 
-        type="text" 
-        placeholder="Search here..." 
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
+  <div>
+    {showing ? <Hello /> : null}
+    <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+  </div>
   );
 }
 
-export default App
+
+export default App;
